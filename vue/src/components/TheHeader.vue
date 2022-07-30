7708e14f8519fa932869282edee9b22bafac61bc
Miklakapi <kkmiklaszewski@gmail.com>
2022-07-30 11:24:23 +0000

    header

---
+++ vue/src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">
+        Bill Spliter
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    text-align: center;
+    padding: 20px;
+    font-weight: 25px;
+    font-weight: bold;
+    letter-spacing: 3px;
+}
+</style>
