fb593d041c7440f853d6fcce6c56df10d05b7e94
Miklakapi <kkmiklaszewski@gmail.com>
2022-07-30 19:27:04 +0000

    results

--- vue/src/components/Results.vue
+++ vue/src/components/Results.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="row">
         <div class="col-5">
-            <div>Bill sum: {{ data.billSum }} $</div>
-            <div>Products: {{ data.numberOfProducts }}</div>
-            <div class="mt-3">User sum: {{ data.userSum }} $</div>
-            <div>Users: {{ data.numberOfUsers }}</div>
+            <div><b>Bill sum:</b> {{ data.billSum }} $</div>
+            <div><b>Products:</b> {{ data.numberOfProducts }}</div>
+            <div class="mt-3"><b>User sum:</b> {{ data.userSum }} $</div>
+            <div><b>Users:</b> {{ data.numberOfUsers }}</div>
         </div>
         <div class="col-7">
             <div v-for="user in user_list" :key="user.id">
-                {{ user.name }}: {{ user.sum }}$
+                <b>{{ user.name }}:</b> {{ user.sum }}$
             </div>
         </div>
     </section>
