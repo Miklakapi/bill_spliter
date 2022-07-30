bc831bfc37b330c43932f932cef4c828132dc227
Miklakapi <kkmiklaszewski@gmail.com>
2022-07-30 11:41:05 +0000

    button section

---
+++ vue/src/components/TheAddForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="button-container row text-center mb-3 mt-2">
+        <div class="col-6">
+            <add-button>Add product</add-button>
+        </div>
+        <div class="col-6 text-center">
+            <add-button>Add user</add-button>
+        </div>
+    </div>
+</template>
+
+<script>
+import AddButton from './AddButton.vue';
+
+export default {
+    components: {
+        AddButton,
+    }
+}
+</script>
