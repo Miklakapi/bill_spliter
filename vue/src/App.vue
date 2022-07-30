96008d4ab5a70121e3457553ad50bc66cdf20d18
Miklakapi <kkmiklaszewski@gmail.com>
2022-07-30 11:21:58 +0000

    App

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,35 @@
 <template>
-    <div>123</div>
+    <div class="container p-0">
+        <the-header></the-header>
+        <div class="content">
+
+        </div>
+    </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 
+export default {
+    components: {
+        TheHeader,
+    }
+}
 </script>
 
-<style scoped>
+<style lang="scss">
 body {
-    background: red;
+    background-image: url("./black-1920.jpg");
+    margin: 0;
+    color: #bec5cb;
+}
+
+.container {
+    background-color: #353c42;
+    box-shadow: 0 0 10px rgb(0 0 0 / 50%);
+}
+
+.content {
+    display: flex;
 }
 </style>
