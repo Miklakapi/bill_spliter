5eec3cdf7dd66690e20b7b1dd0d9062d9c81a4ec
Miklakapi <kkmiklaszewski@gmail.com>
2022-07-30 16:52:41 +0000

    results popup

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="container p-0 vh-100">
+        <dialog-popup v-if="dialogPopup.status" @close="dialogPopup.status=false">
+            <template v-slot:head>{{ dialogPopup.head }}</template>
+            <template v-slot:default>
+                <results :data="dialogPopup.data" :user_list="user_list"></results>
+            </template>
+        </dialog-popup>
         <the-header></the-header>
         <the-add-form 
             @product="product" 
@@ -24,21 +30,28 @@ import TheHeader from './components/TheHeader.vue';
 import TheAddForm from './components/TheAddForm.vue';
 import TheTable from './components/TheTable.vue';
 import Box from './components/Box.vue';
+import DialogPopup from './components/DialogPopup.vue';
 import TheFooter from './components/TheFooter.vue';
+import Results from './components/Results.vue';
 
 export default {
     data() {
         return {
             user_list: [
-                {id: 1, name: 'user1', sum: 0},
-                {id: 2, name: 'user2', sum: 0},
-                {id: 3, name: 'user3', sum: 0},
+                {id: 1, name: 'user1', sum: 1},
+                {id: 2, name: 'user2', sum: 2},
+                {id: 3, name: 'user3', sum: 3},
                 ],
             product_list: [
                 {id: 1, name: 'milk', price: 2.45},
                 {id: 2, name: 'pizza', price: 10.12},
                 {id: 3, name: 'water', price: 1.28},
             ],
+            dialogPopup: {
+                status: false,
+                head: '',
+                data: null,
+            },
         };
     },
     components: {
@@ -46,17 +59,34 @@ export default {
         TheAddForm,
         TheTable,
         Box,
+        TheFooter,
+        DialogPopup,
         Results,
     },
     methods: {
         calculate() {
-            let sum = 0;
-            let numberOfProduct = 0;
+            let billSum = 0;
+            let numberOfProducts = 0;
             this.product_list.forEach(element => {
-                sum += element.price;
-                numberOfProduct++;
+                billSum += element.price;
+                numberOfProducts++;
             });
-            alert(sum.toFixed(2));
+
+            let userSum = 0;
+            let numberOfUsers = 0;
+            this.user_list.forEach(element => {
+                userSum += element.sum;
+                numberOfUsers++;
+            });
+
+            this.dialogPopup.head = 'Results';
+            this.dialogPopup.data = { 
+                billSum: billSum.toFixed(2), 
+                numberOfProducts: numberOfProducts, 
+                userSum: userSum.toFixed(2),
+                numberOfUsers: numberOfUsers,
+            };
+            this.dialogPopup.status = true;
         },
         reset() {
             this.product_list.splice(0, this.product_list.length);
@@ -68,7 +98,7 @@ export default {
         },
         user(data) {
             const lastId = this.user_list.at(-1).id;
-            this.user_list.push({id: lastId + 1, name: 'new user'});
+            this.user_list.push({id: lastId + 1, name: 'new user', sum: 0});
         },
         deleteColumn(id) {
             this.user_list.splice(this.user_list.findIndex(function(element) {
---
+++ vue/src/components/Results.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="row">
+        <div class="col-5">
+            <div>Bill sum: {{ data.billSum }} $</div>
+            <div>Products: {{ data.numberOfProducts }}</div>
+            <div class="mt-3">User sum: {{ data.userSum }} $</div>
+            <div>Users: {{ data.numberOfUsers }}</div>
+        </div>
+        <div class="col-7">
+            <div v-for="user in user_list" :key="user.id">
+                {{ user.name }}: {{ user.sum }}$
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['data', 'user_list'],
+}
+</script>
